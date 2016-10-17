2f3cf838846eaa074bc62ab8f67208e0d81a89a9
naoya <mine@mine-studio.com>
2016-10-17 12:37:45 +0900

    インクルード項目の修正

--- opencv_test/opencv_test/videotest.h
+++ opencv_test/opencv_test/videotest.h
@@ -1,8 +1,7 @@
 #ifndef OPENCV_TEST_VIDEOTEST_H_
 #define OPENCV_TEST_VIDEOTEST_H_
 
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 class VideoTest {
 public:
