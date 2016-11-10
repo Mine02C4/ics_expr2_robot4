e464200b320dfe73361caf2d929819bbf5017811
naoya <mine@mine-studio.com>
2016-11-10 15:54:32 +0900

    ORB特徴量検出 refs #4

--- opencv_test/opencv_test/videotest.cpp
+++ opencv_test/opencv_test/videotest.cpp
@@ -1,5 +1,9 @@
 #include "videotest.h"
 
+#include <iostream>
+
+#include <opencv2/features2d.hpp>
+
 VideoTest::VideoTest()
 {
 }
@@ -19,8 +23,9 @@ int VideoTest::Init()
   cap_.set(CV_CAP_PROP_FRAME_WIDTH, 320);
   cap_.set(CV_CAP_PROP_FRAME_HEIGHT, 240);
   cv::namedWindow("VideoTest", CV_WINDOW_AUTOSIZE);
+  cv::namedWindow("Output", CV_WINDOW_AUTOSIZE);
   cap_.read(frame_);
-  disp_img_.create(cv::Size(320, 240), CV_8UC3);
+  //disp_img_.create(cv::Size(320, 240), CV_8UC3);
   return 0;
 }
 
@@ -32,3 +37,25 @@ void VideoTest::ReadFrame()
     return;
   cv::imshow("VideoTest", frame_);
 }
+
+void VideoTest::TrackFeatures()
+{
+  cv::Ptr<cv::Feature2D> detector, extractor;
+  detector = cv::FeatureDetector::create("ORB");
+  extractor = cv::FeatureDetector::create("ORB");
+  std::vector<cv::KeyPoint> kpts;
+  cv::Mat desc;
+  detector->detect(frame_, kpts);
+  extractor->compute(frame_, kpts, desc);
+  if (desc.rows == 0) {
+    std::cout << "No feature." << std::endl;
+    return;
+  }
+  disp_features_ = frame_;
+  for (int i = 0; i < kpts.size(); ++i) {
+    cv::circle(disp_features_, kpts[i].pt, 5, cv::Scalar(200, 0, 0), 1, CV_AA);
+  }
+  cv::imshow("Output", disp_features_);
+}
+
+
--- opencv_test/opencv_test/videotest.h
+++ opencv_test/opencv_test/videotest.h
@@ -9,10 +9,11 @@ public:
   ~VideoTest();
   int Init();
   void ReadFrame();
+  void TrackFeatures();
 private:
   cv::Mat gray_img_;
   cv::VideoCapture cap_;
-  cv::Mat frame_, disp_img_;
+  cv::Mat frame_, disp_features_;
 };
 
 #endif  // OPENCV_TEST_VIDEOTEST_H_
