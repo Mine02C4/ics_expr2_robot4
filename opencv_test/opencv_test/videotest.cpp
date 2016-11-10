4ef25569bc237d0d84f888c5c4c6738a470891b2
naoya <mine@mine-studio.com>
2016-11-10 17:59:53 +0900

    特徴点マッチングでの物体追跡

--- opencv_test/opencv_test/videotest.cpp
+++ opencv_test/opencv_test/videotest.cpp
@@ -2,8 +2,6 @@
 
 #include <iostream>
 
-#include <opencv2/features2d.hpp>
-
 VideoTest::VideoTest()
 {
 }
@@ -45,15 +43,51 @@ void VideoTest::TrackFeatures()
   extractor = cv::FeatureDetector::create("ORB");
   std::vector<cv::KeyPoint> kpts;
   cv::Mat desc;
+
+  // 特徴点検出と特徴量記述
   detector->detect(frame_, kpts);
   extractor->compute(frame_, kpts, desc);
   if (desc.rows == 0) {
     std::cout << "No feature." << std::endl;
     return;
   }
+
+  // 特徴点マッチング
+  cv::BFMatcher matcher;
+  std::vector<std::vector<cv::DMatch>> knn_matches;
+  matcher.knnMatch(prev_desc_, desc, knn_matches, 2);
+
+  const auto match_par = .6f;
+  std::vector<cv::DMatch> good_matches;
+  std::vector<cv::Point2f> match_point1;
+  std::vector<cv::Point2f> match_point2;
+  bool flag = false;
+  for (int i = 0; i < knn_matches.size(); ++i) {
+    if (knn_matches[i].size() == 1) {
+      flag = true;
+    }
+    else if (knn_matches[i].size() > 1) {
+      auto dist1 = knn_matches[i][0].distance;
+      auto dist2 = knn_matches[i][1].distance;
+      flag = dist1 <= dist2 * match_par;
+    }
+    if (flag) {
+      good_matches.push_back(knn_matches[i][0]);
+      match_point1.push_back(prev_keypts_[knn_matches[i][0].queryIdx].pt);
+      match_point2.push_back(kpts[knn_matches[i][0].trainIdx].pt);
+    }
+  }
+
+  prev_keypts_ = kpts;
+  prev_desc_ = desc;
+
   disp_features_ = frame_;
   for (int i = 0; i < kpts.size(); ++i) {
-    cv::circle(disp_features_, kpts[i].pt, 5, cv::Scalar(200, 0, 0), 1, CV_AA);
+    cv::circle(disp_features_, kpts[i].pt, 2, cv::Scalar(200, 0, 0), 1, CV_AA);
+  }
+  for (int i = 0; i < match_point1.size(); i++) {
+    cv::line(disp_features_, match_point1[i], match_point2[i], cv::Scalar(0, 200, 0), 2, CV_AA);
+    cv::circle(disp_features_, match_point2[i], 4, cv::Scalar(0, 200, 0), 1, CV_AA);
   }
   cv::imshow("Output", disp_features_);
 }
--- opencv_test/opencv_test/videotest.h
+++ opencv_test/opencv_test/videotest.h
@@ -2,6 +2,7 @@
 #define OPENCV_TEST_VIDEOTEST_H_
 
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d.hpp>
 
 class VideoTest {
 public:
@@ -14,6 +15,8 @@ private:
   cv::Mat gray_img_;
   cv::VideoCapture cap_;
   cv::Mat frame_, disp_features_;
+  std::vector<cv::KeyPoint> prev_keypts_;
+  cv::Mat prev_desc_;
 };
 
 #endif  // OPENCV_TEST_VIDEOTEST_H_
