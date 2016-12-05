e88302175bc7d6387fb39b7ed3245ca770cb5bfa
torinokocity01 <yuutairoiro@gmail.com>
2016-12-05 16:32:36 +0900

    ステッピングモータの回転角制御
    1度単位での回転角制御、ラズパイとの通信受信機能（未テスト）

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,9 +1,17 @@
+#include <SPI.h>
 void setup() {
-  // put your setup code here, to run once:
+ 
 
+  // put your setup code here, to run once:
+  setup_gun();
+  setup_motor();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+ // loop_gun();
 
+  loop_motor();
+
+  
 }
