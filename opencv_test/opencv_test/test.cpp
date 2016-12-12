891a9f4a13d593fdafa59f2960014eb47920f090
naoya <mine@mine-studio.com>
2016-12-12 16:11:12 +0900

    テストモードの分岐機能を実装

--- opencv_test/opencv_test/test.cpp
+++ opencv_test/opencv_test/test.cpp
@@ -9,19 +9,37 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-  VideoTest* vt = new VideoTest();
-  vt->Init();
+  int mode;
+  cout << "Select mode" << endl;
+  cout << "1: Feature detection test" << endl;
+  cout << "2: Calibration" << endl;
+  cout << "3: Object detection by color" << endl;
+  cin >> mode;
+  switch (mode)
+  {
+  case 1:
+  {
+    VideoTest* vt = new VideoTest();
+    vt->Init();
 
-  while (1) {
-    vt->ReadFrame();
-    vt->TrackFeatures();
-    int key = waitKey(1);
-    if (key >= 0) {
-      break;
+    while (1) {
+      vt->ReadFrame();
+      vt->TrackFeatures();
+      int key = waitKey(1);
+      if (key >= 0) {
+        break;
+      }
     }
+    delete vt;
+    break;
   }
-
-  delete vt;
+  case 2:
+    break;
+  case 3:
+    break;
+  default:
+    break;
+  }
 
   return 0;
 }
