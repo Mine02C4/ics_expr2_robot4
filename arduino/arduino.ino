5df664113aa1a330a2696c4e22aebbc26e61b555
torinokocity01 <yuutairoiro@gmail.com>
2016-12-19 14:41:39 +0900

    fire命令の確認

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,17 +1,12 @@
 #include <SPI.h>
 void setup() {
- 
-
-  // put your setup code here, to run once:
-  setup_gun();
-  setup_motor();
+ setup_gun();
+ setup_motor();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
- // loop_gun();
-
-  loop_motor();
+ //test();
+ loop_motor();
 
   
 }
--- arduino/gun_control.ino
+++ arduino/gun_control.ino
@@ -1,50 +1,24 @@
-//#include <SPI.h>
 void setup_gun() {
-  // put your setup code here, to run once:
-pinMode(7,OUTPUT); 
-Serial.begin(9600);
-   delay(10000);
-Serial.println("set up finished");
-
-// motor
-
+  pinMode(7,OUTPUT); 
+  Serial.begin(9600);
+  delay(10000);
+  Serial.println("set up finished");
 }
 
 void loop_gun() {
- 
- /* Serial.println("5");
-    delay(1000); Serial.println("4");
-    delay(1000); Serial.println("3");
-    delay(1000); Serial.println("2");
-    delay(1000); Serial.println("1");
-    delay(1000);*/
-  // put your main code here, to run repeatedly:
-digitalWrite(7,HIGH);//  Serial.println("high");
-
-  delay(150);
-  digitalWrite(7,LOW); // Serial.println("low");
-    //delay(1000);
+  digitalWrite(7,HIGH);
+  delay(1500);
+  
+  digitalWrite(7,LOW); 
 }
 
 void loop_gun_n(int n) {
-   Serial.println("start");
-    delay(1000);
-  //    Serial.println("10");
-   // delay(1000); Serial.println("9");
-  //  delay(1000); Serial.println("8");
-//    delay(1000); Serial.println("7");
-//    delay(1000); Serial.println("6");
-//    delay(1000);
- /* Serial.println("5");
-    delay(1000); Serial.println("4");
-    delay(1000); Serial.println("3");
-    delay(1000); Serial.println("2");
-    delay(1000); Serial.println("1");
-    delay(1000);*/
-  // put your main code here, to run repeatedly:
-digitalWrite(7,HIGH);//  Serial.println("high");
-
+  digitalWrite(7,HIGH);
   delay(150 * n);
-  digitalWrite(7,LOW); // Serial.println("low");
-    //delay(1000);
+  digitalWrite(7,LOW); 
+  }
+
+void test(){
+  digitalWrite(7,HIGH);
 }
+
