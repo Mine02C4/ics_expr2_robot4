20a6a457545938c46c28c63366969495c6a26d39
naoya <mine@mine-studio.com>
2016-12-20 20:19:57 +0900

    ステレオカメラキャリブレーション実装(テストはできてない) refs #3

--- opencv_test/opencv_test/test.cpp
+++ opencv_test/opencv_test/test.cpp
@@ -286,7 +286,6 @@ int main(int argc, char** argv)
 
     if (files_exist) {
       int input;
-      Size imageSize = src_image[0].size();
       cout << "Stereo chessboard images are already exist." << endl;
       cout << "1: Use the images" << endl;
       cout << "2: Take new images" << endl;
@@ -294,6 +293,7 @@ int main(int argc, char** argv)
       switch (input)
       {
       case 1:
+      {
         for (int i = 0; i < N_BOARDS * 2; i++) {
           vector<Point2f> imageCorners;
           Mat dst_image, gray_image;
@@ -302,48 +302,8 @@ int main(int argc, char** argv)
           cornerSubPix(gray_image, imageCorners, Size(9, 9), Size(-1, -1), TermCriteria(CV_TERMCRIT_ITER + CV_TERMCRIT_EPS, 30, 0.1));
           imagePoints[i % 2].push_back(imageCorners);
         }
-        for (int i = 0; i < N_BOARDS; i++) {
-          objectPoints.push_back(objectCorners);
-        }
-        double rms = stereoCalibrate(objectPoints, imagePoints[0], imagePoints[1],
-          cameraMatrix[0], distCoeffs[0],
-          cameraMatrix[1], distCoeffs[1],
-          imageSize, R, T, E, F,
-          CALIB_FIX_ASPECT_RATIO +
-          CALIB_ZERO_TANGENT_DIST +
-          CALIB_USE_INTRINSIC_GUESS +
-          CALIB_SAME_FOCAL_LENGTH +
-          CALIB_RATIONAL_MODEL +
-          CALIB_FIX_K3 + CALIB_FIX_K4 + CALIB_FIX_K5,
-          TermCriteria(TermCriteria::COUNT + TermCriteria::EPS, 100, 1e-5));
-        cout << "done with RMS error=" << rms << endl;
-
-        // Error check
-        double err = 0;
-        int npoints = 0;
-        vector<Vec3f> lines[2];
-        for (int i = 0; i < N_BOARDS; i++)
-        {
-          int npt = (int)imagePoints[0][i].size();
-          Mat imgpt[2];
-          for (int k = 0; k < 2; k++)
-          {
-            imgpt[k] = Mat(imagePoints[k][i]);
-            undistortPoints(imgpt[k], imgpt[k], cameraMatrix[k], distCoeffs[k], Mat(), cameraMatrix[k]);
-            computeCorrespondEpilines(imgpt[k], k + 1, F, lines[k]);
-          }
-          for (int j = 0; j < npt; j++)
-          {
-            double errij = fabs(imagePoints[0][i][j].x*lines[1][j][0] +
-              imagePoints[0][i][j].y*lines[1][j][1] + lines[1][j][2]) +
-              fabs(imagePoints[1][i][j].x*lines[0][j][0] +
-                imagePoints[1][i][j].y*lines[0][j][1] + lines[0][j][2]);
-            err += errij;
-          }
-          npoints += npt;
-        }
-        cout << "average epipolar err = " << err / npoints << endl;
         break;
+      }
       case 2:
         for (int i = 0; i < N_BOARDS * 2; ++i) {
           src_image[i].release();
@@ -355,6 +315,119 @@ int main(int argc, char** argv)
         exit(1);
       }
     }
+
+    if (!files_exist) {
+      Mat frame0, frame1;
+      VideoCapture cap[2];
+      cap[0].open(0);
+      cap[1].open(1);
+      if (!cap[0].isOpened() || !cap[1].isOpened()) {
+        cerr << "Cannot open camera." << endl;
+        exit(1);
+      }
+      for (int i = 0; i < N_BOARDS; i++) {
+        bool capture_phase = true;
+        while (capture_phase) {
+          cap[0] >> frame0;
+          cap[1] >> frame1;
+          imshow("chessboard camera 0", frame0);
+          imshow("chessboard camera 1", frame1);
+          int key = waitKey(10);
+          switch (key)
+          {
+          case 13: // Enter
+            src_image[i * 2] = frame0.clone();
+            src_image[i * 2 + 1] = frame1.clone();
+            capture_phase = false;
+            break;
+          case 27: // Esc
+            cout << "Canceled" << endl;
+            return 0;
+          default:
+            break;
+          }
+        }
+        {
+          vector<Point2f> imageCorners[2];
+          Mat cboard_preview[2], gray_image[2];
+          bool found[2];
+          found[0] = findChessboardCorners(src_image[i * 2], BOARD_SIZE, imageCorners[0]);
+          cvtColor(src_image[i * 2], gray_image[0], CV_BGR2GRAY);
+          cornerSubPix(gray_image[0], imageCorners[0], Size(9, 9), Size(-1, -1), TermCriteria(CV_TERMCRIT_ITER + CV_TERMCRIT_EPS, 30, 0.1));
+          found[1] = findChessboardCorners(src_image[i * 2 + 1], BOARD_SIZE, imageCorners[1]);
+          cvtColor(src_image[i * 2 + 1], gray_image[1], CV_BGR2GRAY);
+          cornerSubPix(gray_image[1], imageCorners[1], Size(9, 9), Size(-1, -1), TermCriteria(CV_TERMCRIT_ITER + CV_TERMCRIT_EPS, 30, 0.1));
+
+          cboard_preview[0] = src_image[i * 2].clone();
+          cboard_preview[1] = src_image[i * 2 + 1].clone();
+          drawChessboardCorners(cboard_preview[0], BOARD_SIZE, imageCorners[0], found);
+          drawChessboardCorners(cboard_preview[1], BOARD_SIZE, imageCorners[1], found);
+          imshow("chessboard camera 0", cboard_preview[0]);
+          imshow("chessboard camera 1", cboard_preview[1]);
+          int key = waitKey(0);
+          switch (key)
+          {
+          case 13: // Enter
+            imwrite("stereo_cboard_" + to_string(i) + "_0.png", src_image[i * 2]);
+            imwrite("stereo_cboard_" + to_string(i) + "_1.png", src_image[i * 2 + 1]);
+            imagePoints[0].push_back(imageCorners[0]);
+            imagePoints[1].push_back(imageCorners[1]);
+            break;
+          case 32: // Space
+            --i;
+            break;
+          case 27: // Esc
+            cout << "Canceled" << endl;
+            return 0;
+          default:
+            break;
+          }
+        }
+      }
+    }
+
+    Size imageSize = src_image[0].size();
+    for (int i = 0; i < N_BOARDS; i++) {
+      objectPoints.push_back(objectCorners);
+    }
+    double rms = stereoCalibrate(objectPoints, imagePoints[0], imagePoints[1],
+      cameraMatrix[0], distCoeffs[0],
+      cameraMatrix[1], distCoeffs[1],
+      imageSize, R, T, E, F,
+      CALIB_FIX_ASPECT_RATIO +
+      CALIB_ZERO_TANGENT_DIST +
+      CALIB_USE_INTRINSIC_GUESS +
+      CALIB_SAME_FOCAL_LENGTH +
+      CALIB_RATIONAL_MODEL +
+      CALIB_FIX_K3 + CALIB_FIX_K4 + CALIB_FIX_K5,
+      TermCriteria(TermCriteria::COUNT + TermCriteria::EPS, 100, 1e-5));
+    cout << "done with RMS error=" << rms << endl;
+
+    // Error check
+    double err = 0;
+    int npoints = 0;
+    vector<Vec3f> lines[2];
+    for (int i = 0; i < N_BOARDS; i++)
+    {
+      int npt = (int)imagePoints[0][i].size();
+      Mat imgpt[2];
+      for (int k = 0; k < 2; k++)
+      {
+        imgpt[k] = Mat(imagePoints[k][i]);
+        undistortPoints(imgpt[k], imgpt[k], cameraMatrix[k], distCoeffs[k], Mat(), cameraMatrix[k]);
+        computeCorrespondEpilines(imgpt[k], k + 1, F, lines[k]);
+      }
+      for (int j = 0; j < npt; j++)
+      {
+        double errij = fabs(imagePoints[0][i][j].x*lines[1][j][0] +
+          imagePoints[0][i][j].y*lines[1][j][1] + lines[1][j][2]) +
+          fabs(imagePoints[1][i][j].x*lines[0][j][0] +
+            imagePoints[1][i][j].y*lines[0][j][1] + lines[0][j][2]);
+        err += errij;
+      }
+      npoints += npt;
+    }
+    cout << "average epipolar err = " << err / npoints << endl;
     break;
   }
   case 4:
@@ -374,6 +447,8 @@ int main(int argc, char** argv)
   default:
     break;
   }
+
+
 
   return 0;
 }
