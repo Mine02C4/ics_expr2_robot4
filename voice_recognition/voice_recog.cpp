cc835da832f42260fe34a83a0ef647285c9c792b
sensinkoku <nmoari@gmail.com>
2016-12-25 18:12:58 +0900

    不要コメントの削除・iconvの削除

--- voice_recognition/voice_recog.cpp
+++ voice_recognition/voice_recog.cpp
@@ -2,32 +2,21 @@
 #include <string>
 #include <julius/juliuslib.h>
 #include "voice_recog.hpp"
-//#include <iconv.h>
-//#define STR_OUT 100
-//typedef void (*FUNCTYPE1)(std::string);
+
+// Global variable
 FUNCTYPE1 fp1;
-//Jconf *jconf;
-//Recog *recog;
-//static int ret;
 static char * mine_jconf = {(char *)"dictation-kit-v4.2.3/mine.jconf"};
 static char * fast_jconf = {(char *)"dictation-kit-v4.2.3/fast.jconf"};
 
 int Voicerec::Init() {
   //Initialize
   jlog_set_output(NULL);
-  /*
-  if (argc == 1) {    // -Cオプションで指定する.jconfファイルは必要なので確認する
-    fprintf(stderr, "Try `-help' for more information.\n");
-    return -1;
-    }*/
-
   // 指定した.jconfファイルから設定を読み込む
   jconf  = j_config_load_file_new(fast_jconf);
   if (jconf == NULL) {
     fprintf(stderr, "jconf load miss\n");
     return -1;
   }
-
   // 読み込んだjconfから認識器を作成する
   recog = j_create_instance_from_jconf(jconf);
   if (recog == NULL) {
@@ -39,7 +28,6 @@ int Voicerec::Init() {
     /* error */
     return -1;
   }
-
   // 入力デバイスがちゃんと開けてるか確認
   switch(j_open_stream(recog, NULL)) {
   case 0:     /* succeeded */
@@ -67,16 +55,11 @@ void Voicerec::Output_Result(Recog * recog, void * dummy) {
   int n,i;
   Sentence *s;
   RecogProcess *r;
-  //charconv *cc = new charconv;
-
   for(r=recog->process_list;r;r=r->next) {
-
     // 処理が生きてないときは飛ばす
     if (! r->live) continue;
-
     // 処理結果が得られない時も飛ばす
     if (r->result.status < 0) continue;
-
     // 全てのセンテンスをアウトプットする
     winfo = r->lm->winfo;
     for(n = 0; n < r->result.sentnum; n++) { // センテンスの数だけループ
@@ -85,7 +68,6 @@ void Voicerec::Output_Result(Recog * recog, void * dummy) {
       seqnum = s->word_num;        // ワードの数
       //printf("結果:", n+1);
       for(i=0;i<seqnum;i++) { // ワードの数だけ回す
-	//cc->convert(winfo->woutput[seq[i]], str_out, (size_t)STR_OUT); // ワードをutf8に変換してstr_outに入れる
 	fp1(winfo->woutput[seq[i]]);
       }
       printf("\n");
@@ -93,7 +75,6 @@ void Voicerec::Output_Result(Recog * recog, void * dummy) {
   }
   fflush(stdout);
 }
-
 void Voicerec::Register_Callback(FUNCTYPE1 f) {
   //Register callback
   fp1 = f;
--- voice_recognition/voice_recog.hpp
+++ voice_recognition/voice_recog.hpp
@@ -1,8 +1,8 @@
 #ifndef VOICE_RECOG_H_
 #define VOICE_RECOG_H_
 
-//#include <julius/juliuslib.h>
 typedef void (*FUNCTYPE1)(std::string);
+
 //Forward declaration
 struct __Jconf__;
 struct __Recog__;
@@ -20,10 +20,10 @@ public:
   int Init();
   void Finalize();
 private:
-  //static FUNCTYPE1 fp1;
   Jconf *jconf;
   Recog *recog;
   static void Output_Result(Recog *recog, void * dummy);
   Voicerec();
 };
+
 #endif //VOICE_RECOG_H_
