5335d4d39b25ffb7bb73b27737a77cf22e524583
miyazakikazuki <kazukisaxophone@gmail.com>
2017-01-16 16:24:58 +0900

    近づきすぎると離れる設定を付加

--- core/MainLogic.cpp
+++ core/MainLogic.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 
 #include "CVision.hpp"
+#include "Gun.hpp"
 
 #include "../stub_library/common.h" // TODO: Will be removed.
 
@@ -28,7 +29,7 @@ void MainLogic::Init()
 void MainLogic::Launch()
 {
   // Get command from other interfaces.
-  for (int i = 0; i < 100; ++i) {
+  for (int i = 0; i < 1000; ++i) {
     int area, cx, cy;
     vision_.getInstance().ReadFrame();
     if (vision_.getInstance().DetectBlueBox(area, cx, cy)) {
@@ -42,6 +43,13 @@ void MainLogic::Launch()
       else {
         if (area < 3000) {
           drive_.RunForwardPeriodInSeconds(0.5);
+        }
+        if (area > 5000) {
+          drive_.RunForwardPeriodInSeconds(-0.1);
+        }
+        else{
+        //gun fire
+        //gun_.open_fire(1);
         }
       }
     }
