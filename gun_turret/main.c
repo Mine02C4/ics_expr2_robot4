cd47ff097a2e88f603538374604f6d730e1d7a73
Shoya Matsumori <shoya.matsumori@gmail.com>
2017-01-16 17:00:12 +0900

    MOD: Implement gun control in new protocol.

--- gun_turret/main.c
+++ gun_turret/main.c
@@ -6,7 +6,32 @@ int
 main()
 {
   turret_init();
-  open_fire(1);
+	int input;
+	while (1) {
+		fprintf(stderr, "MODE: (1:fire, 2:turn, 3: elevate)\n");
+		scanf("%d", &input);
+		switch (input) {
+			case 1:
+				fprintf(stderr, "FIRE: (number of bullets)\n");
+				scanf("%d", &input);
+				open_fire(input);
+				break;
+			case 2:
+				fprintf(stderr, "TURN: (degrees)\n");
+				scanf("%d", &input);
+				turn_by_degrees(input);
+				break;
+			case 3:
+				fprintf(stderr, "ELEVATE (degrees)\n");
+				scanf("%d", &input);
+				elevate_by_degrees(input);
+				break;
+
+			default:
+				fprintf(stderr, "Invalid\n");
+				break;
+		}
+	}
   turret_finalize();
   return 0;
 }
