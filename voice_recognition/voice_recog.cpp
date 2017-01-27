994fd920ccb247971d2b37612765998dbf4bc1a2
sensinkoku <nmoari@gmail.com>
2017-01-27 12:12:51 +0900

    Fixed string compare in voice_recog

--- voice_recognition/voice_recog.cpp
+++ voice_recognition/voice_recog.cpp
@@ -156,43 +156,17 @@ int Voicerec::Wait_One_Code(int seconds) {
   return Convert_String_to_Code(Wait_One_Sentence(seconds));
 }
 int Convert_String_to_Code(std::string s) {
-  switch(s) {
-    case "前":
-      return VC_CODE_FORWARD;
-    break;
-    case "後ろ":
-      return VC_CODE_BACK;
-    break;
-    case "右":
-      return VC_CODE_RIGHT;
-    break;
-    case "左":
-      return VC_CODE_LEFT;
-    break;
-    case "進め":
-      return VC_CODE_FORWARD;
-    break;
-    case "行け":
-      return VC_CODE_FORWARD;
-    break;
-    case "まわれ":
-      return VC_CODE_ROTATE;
-    break;
-    case "とまれ":
-      return VC_CODE_STOP;
-    break;
-    case "さがれ":
-      return VC_CODE_BACK;
-    break;
-    case "うて":
-      return VC_CODE_FIRE;
-    break;
-    default
-      return -1;
-    break;
-  
-  }
-  return -1;
+  if (s == "前") return VC_CODE_FORWARD;
+  else if (s == "後ろ")  return VC_CODE_BACK;
+  else if (s == "右")    return VC_CODE_RIGHT;
+  else if (s == "左")    return VC_CODE_LEFT;
+  else if (s == "進め")  return VC_CODE_FORWARD;
+  else if (s == "行け")  return VC_CODE_FORWARD;
+  else if (s == "まわれ") return VC_CODE_ROTATE;   
+  else if (s == "とまれ") return VC_CODE_STOP;
+  else if (s == "さがれ") return VC_CODE_BACK;
+  else if (s == "うて")   return VC_CODE_FIRE;
+  else  return -1;
 }
 Voicerec::Voicerec() :flag(0), result("") {
 }
