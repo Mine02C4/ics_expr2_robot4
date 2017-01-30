f4962bc709ade7c5ee4d32dfc37143e7f853a867
Shoya Matsumori <shoya.matsumori@gmail.com>
2017-01-30 14:03:32 +0900

    MOD: Add Turn command in Drive

--- core/Drive.cpp
+++ core/Drive.cpp
@@ -60,18 +60,29 @@ void Drive::RunForward(double rot)
 
 void Drive::Turn(double angle)
 {
+	if (angle > 0) {
+		TurnLeft(angle);
+	} else (angle < 0) {
+		TurnRight(-angle);
+	}
+	return;
 }
 
 void Drive::TurnRight(double angle)
 {
-  turn_right(angle);
+	int mot = (int) angle * 430;		// converting to mot scale
+	int motv[2] = {mot, 0};
+  turn_right(mot);
+	SetCurrentPosition(motv);
   return;
 }
 
 void Drive::TurnLeft(double angle)
 {
-  static int currangle;
-  turn_left(angle);
+	int mot = (int) angle * 430;		// converting to mot scale
+	int motv[2] = {0, mot};
+  turn_left(mot);
+	SetCurrentPosition(motv);
   return;
 }
 
