3c2a202014164e26e2da5ce4373774b90e89813a
torinokocity01 <yuutairoiro@gmail.com>
2017-02-01 15:02:30 +0900

    add debug code

--- arduino/elevation_test.ino
+++ arduino/elevation_test.ino
@@ -9,17 +9,24 @@ void setup_servo()
   delay(2000);
 	servo.attach(9);
   servo_pos = servo.read();
+   Serial.println("pos read\n");
+  
   int i = servo_pos - 50;
   while(i < 0){
     delay(10);
     servo.write(50 + i);
     i--;
   }
+   Serial.println("first while finished");
+  
   while(i > 0){
     delay(10);
     servo.write(50 + i);
     i++;
   }
+  
+   Serial.println("socond while finished");
+  
   Serial.println("servo set up finished");
   Serial.println(servo_pos);
 }
